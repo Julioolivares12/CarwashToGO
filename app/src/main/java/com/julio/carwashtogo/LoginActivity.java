4f6505139dc85d1223f06ee37b309daf5377e604
julio <julioolivares90@gmail.com>
2019-09-01 17:47:06 -0600

    cambios

--- app/src/main/java/com/julio/carwashtogo/LoginActivity.java
+++ app/src/main/java/com/julio/carwashtogo/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.julio.carwashtogo;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,14 +9,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.julio.carwashtogo.common.Constantes;
 
 public class LoginActivity extends AppCompatActivity {
@@ -24,6 +29,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText edt_correo,edt_password;
 
     private Button btnLogin,btnLoginWithFacebook,btnLoginWithTwitter,btnLoginWithGoogle;
+    private String rol = "";
     FirebaseAuth mAuth;
     FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
     DatabaseReference databaseReference = firebaseDatabase.getReference(Constantes.USER_CHILD);
@@ -57,6 +63,7 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String correo = edt_correo.getText().toString().trim();
                 String password = edt_password.getText().toString().trim();
+                login(correo,password);
 
             }
         });
@@ -69,14 +76,29 @@ public class LoginActivity extends AppCompatActivity {
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isSuccessful()){
                     FirebaseUser usuarioActual = mAuth.getCurrentUser();
-
-                    updateUI(usuarioActual);
+                    if (usuarioActual != null){
+                        updateUI(usuarioActual);
+                    }
                 }
             }
         });
     }
 
     private void updateUI(FirebaseUser usuarioActual) {
-        databaseReference.child(Constantes.USER_CHILD).child("");
+        databaseReference.child(Constantes.USER_CHILD).child(usuarioActual.getUid()).child("rol").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                rol = dataSnapshot.getValue(String.class);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(LoginActivity.this, "ocurrio un error "+databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        Intent irPrincipal = new Intent(getApplicationContext(),PrincipalActivity.class);
+        irPrincipal.putExtra(Constantes.ROL_USER,rol);
+        startActivity(irPrincipal);
+        finish();
     }
 }
