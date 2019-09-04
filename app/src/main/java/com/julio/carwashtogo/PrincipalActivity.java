5ad6dfc0cb474d2f0791eaa818d8d267fe22fad4
julio <julioolivares90@gmail.com>
2019-09-03 20:48:33 -0600

    cambios en el menu , creados los fragments que se utilizaran

--- app/src/main/java/com/julio/carwashtogo/PrincipalActivity.java
+++ app/src/main/java/com/julio/carwashtogo/PrincipalActivity.java
@@ -23,6 +23,8 @@ public class PrincipalActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private Bundle datos;
+    private NavController navController;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,13 +41,20 @@ public class PrincipalActivity extends AppCompatActivity {
             }
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         //mAppBarConfiguration = new AppBarConfiguration.Builder(R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_tools, R.id.nav_share, R.id.nav_send).setDrawerLayout(drawer).build();
 
+        mAppBarConfiguration = new AppBarConfiguration.Builder(R.id.nav_listar_empresas,
+                R.id.nav_crear_empresas,
+                R.id.nav_agregar_promociones,
+                R.id.nav_lista_encargados,
+                R.id.nav_nuevo_encargado,
+                R.id.nav_perfilFragment
+        ).setDrawerLayout(drawer).build();
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
 
@@ -54,20 +63,41 @@ public class PrincipalActivity extends AppCompatActivity {
             //hacer cambio de usuaio
             String rol = datos.getString(Constantes.ROL_USER);
             assert rol != null;
-            switch (rol){
-                case "cliente":
-
-                    break;
-                case "otro":
-
-                    break;
-                case "admnistrador":
-
-                    break;
-            }
+            hideOptionMenu(rol);
         }
     }
 
+    private void hideOptionMenu(String tipoUser){
+        Menu menu = navigationView.getMenu();
+        switch (tipoUser){
+            case "cliente":
+                //administrador desactivados
+                menu.findItem(R.id.nav_agregar_promociones).setVisible(false);
+                menu.findItem(R.id.nav_crear_empresas).setVisible(false);
+                menu.findItem(R.id.nav_lista_encargados).setVisible(false);
+                menu.findItem(R.id.nav_nuevo_encargado).setVisible(false);
+
+                //cliente activados
+                menu.findItem(R.id.nav_cuponera).setVisible(true);
+                menu.findItem(R.id.nav_add_vehiculo).setVisible(true);
+                menu.findItem(R.id.nav_catalogo_productos).setVisible(true);
+                break;
+            case "administrador":
+                //cliente desactivados
+                menu.findItem(R.id.nav_cuponera).setVisible(false);
+                menu.findItem(R.id.nav_add_vehiculo).setVisible(false);
+                menu.findItem(R.id.nav_catalogo_productos).setVisible(false);
+
+                //administrador
+                menu.findItem(R.id.nav_agregar_promociones).setVisible(true);
+                menu.findItem(R.id.nav_crear_empresas).setVisible(true);
+                menu.findItem(R.id.nav_lista_encargados).setVisible(true);
+                menu.findItem(R.id.nav_nuevo_encargado).setVisible(true);
+                break;
+            case "encargado":
+                break;
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
